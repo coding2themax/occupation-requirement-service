91f8c7928526321edba0a8b543f22e21879d664e
max quinones <maximilian.quinones@gmail.com>
2024-11-01 20:46:57 -0700

    Init commit

---
+++ occupation-requirement-service/src/main/java/com/coding2themax/career/occupation_requirement_service/OccupationRequirementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.occupation_requirement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupationRequirementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccupationRequirementServiceApplication.class, args);
+	}
+
+}
---
+++ occupation-requirement-service/src/test/java/com/coding2themax/career/occupation_requirement_service/OccupationRequirementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.occupation_requirement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupationRequirementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

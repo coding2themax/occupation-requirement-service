af8e194c5af92d5cd9a52d69728d9c4ead067c46
max quinones <maximilian.quinones@gmail.com>
2024-11-17 16:18:23 -0700

    hello endpoint with RestController

---
+++ occupation-service/src/main/java/com/coding2themax/career/occupation_service/OccupationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.occupation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccupationServiceApplication.class, args);
+	}
+
+}
---
+++ occupation-service/src/test/java/com/coding2themax/career/occupation_service/OccupationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.occupation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

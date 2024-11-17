005d807e47db05673238fb5df12bcef8b66f631e
max quinones <maximilian.quinones@gmail.com>
2024-11-17 15:27:11 -0700

    updates :

--- occupation-requirement-service/src/main/java/com/coding2themax/career/occupation/requirement/OccupationRequirementServiceApplication.java
+++ occupation-requirement-service/src/main/java/com/coding2themax/career/occupation/requirement/OccupationRequirementServiceApplication.java
@@ -3,8 +3,7 @@ package com.coding2themax.career.occupation.requirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.coding2themax.persistance.occupation.service.*",
-		"com.coding2themax.career.occupation.requirement.*" })
+@SpringBootApplication
 public class OccupationRequirementServiceApplication {
 
 	public static void main(String[] args) {
--- occupation-requirement-service/src/main/java/com/coding2themax/career/occupation/requirement/controller/OccupationController.java
+++ occupation-requirement-service/src/main/java/com/coding2themax/career/occupation/requirement/controller/OccupationController.java
@@ -5,32 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import com.coding2themax.persistance.occupation.model.Occupation;
-import com.coding2themax.persistance.occupation.service.OccupationRepoService;
-
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class OccupationController {
-
-  private final OccupationRepoService occupationService;
-
-  public OccupationController(OccupationRepoService occupationService) {
-    this.occupationService = occupationService;
-  }
-
-  @GetMapping("/occupations")
-  public Flux<Occupation> getAllOccupations() {
-    return occupationService.findAll();
-  }
-
-  @GetMapping("/occupations/{id}")
-  public Mono<ServerResponse> getOccupation(ServerRequest request) {
-    return occupationService.findById(Long.parseLong(request.pathVariable("id")))
-        .flatMap(occupation -> ServerResponse.ok().bodyValue(occupation))
-        .switchIfEmpty(ServerResponse.notFound().build());
-  }
 
   @GetMapping("/hello")
   public Mono<ServerResponse> helloWorld(ServerRequest request) {
